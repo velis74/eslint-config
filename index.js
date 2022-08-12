b39c966f130f122f68557047e086328455fa9748
Jure Erznožnik <jure@velis.si>
2022-08-12 14:25:06 +0200

    Allow for ... of loops (no-restricted-syntax)
    This overrides AirBnB style. I basically copied the style and removed the 'ForOfStatement' setting

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ module.exports = {
     'no-underscore-dangle': ['error', { 'allowAfterThis': true }],
     'operator-linebreak': ['error', 'after'],
     'linebreak-style': ['off'], // handled by GIT
+    'no-restricted-syntax': [
+      'error',
+      { selector: 'ForInStatement', message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.', },
+      { selector: 'LabeledStatement', message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.', },
+      { selector: 'WithStatement', message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.', },
+    ],
     'vue/html-closing-bracket-spacing': ['error', { 'selfClosingTag': 'never' }],
     'vue/no-v-html': ['off'],
     'vue/max-attributes-per-line': ['warn', { 'singleline': { 'max': 10 } }],
