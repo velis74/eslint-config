99a2ea4c4af6416defb1717b93908a3a37d6f83c
Jure Erznožnik <jure@velis.si>
2025-09-30 08:52:58 +0200

    final modifications for the new linter rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -108,6 +108,7 @@ export default [
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
 
       // Import rules
       'sort-imports': 'off',
